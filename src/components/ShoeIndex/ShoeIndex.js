657796816bbe74c3ef51f1d8d1f72e06fc13e5bc
reaganmac92 <reaganmccaffrey@gmail.com>
2021-08-27 10:54:08 -0500

    :recycle: Refactor exercise 3 to preferred solution
    Note: Originally I swapped the placement of MainColumn and
    LeftColumn because I thought that were I using a keyboard
    to navigate I would want to go through the types of shoes
    first. However after listening to Josh's explanation I
    think it does make sense to use `row-reverse` here to make
    the ShoeGrid the first thing you can tab through.

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -12,15 +12,6 @@ import ShoeGrid from "../ShoeGrid";
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href='/'>Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href='/sale'>Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href='/sale/shoes'>Shoes</Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
       <MainColumn>
         <Header>
           <Title>Running</Title>
@@ -35,13 +26,24 @@ const ShoeIndex = ({ sortId, setSortId }) => {
         <Spacer size={34} />
         <ShoeGrid />
       </MainColumn>
+      <LeftColumn>
+        <Breadcrumbs>
+          <Breadcrumbs.Crumb href='/'>Home</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/sale'>Sale</Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href='/sale/shoes'>Shoes</Breadcrumbs.Crumb>
+        </Breadcrumbs>
+        <Spacer size={42} />
+        <ShoeSidebar />
+      </LeftColumn>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: row-reverse;
   align-items: baseline;
+  gap: 32px;
 `;
 
 const LeftColumn = styled.div`
@@ -57,6 +59,7 @@ const MainColumn = styled.div`
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
+  align-items: baseline;
 `;
 
 const Title = styled.h2`
